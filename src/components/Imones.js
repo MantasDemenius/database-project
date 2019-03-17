e26621590e6777b598a7179d51206ba04230cbd2
Tidish <tidish@inbox.lt>
2019-03-17 13:37:13 +0200

    initial commit, pradetas darbas

---
+++ src/components/Imones.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Imones extends Component {
+
+
+  render() {
+    return (
+      <div >
+        <h1>Imones</h1>
+      </div>
+    );
+  }
+}
+
+export default Imones;
---
+++ src/components/constants/Array.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const imones = () => {
+[
+  {
+    id: 1,
+    title: sijonai
+  },
+  {
+    id: 2,
+    title: balvonai
+  },
+  {
+    id: 3,
+    title: klijai
+  },
+  {
+    id: 4,
+    title: kelnes
+  }
+]
+}
+
+export default imones;
