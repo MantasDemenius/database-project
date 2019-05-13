8bdd8533f2213d771153fde4776df1cd87f89da1
Tidish <tidish@inbox.lt>
2019-05-13 21:16:25 +0300

    Updates

--- precache-manifest.be46cc4b649fcc5a54b7dc705a4afa1f.js
+++ precache-manifest.be46cc4b649fcc5a54b7dc705a4afa1f.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "c5dc0f160b5204e5736e",
+    "revision": "f037c387670ffb79d976",
     "url": "/database-project/static/css/main.f5604eca.chunk.css"
   },
   {
@@ -20,16 +20,12 @@ self.__precacheManifest = [
     "url": "/database-project/static/js/2.9145d352.chunk.js"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/database-project/static/media/outline-icons.ef60a4f6.woff"
-  },
-  {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "/database-project/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/database-project/static/media/flags.9c74e172.png"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/database-project/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -40,12 +36,16 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/outline-icons.ad97afd3.ttf"
   },
   {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/database-project/static/media/flags.9c74e172.png"
+  },
+  {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/database-project/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "c5dc0f160b5204e5736e",
-    "url": "/database-project/static/js/main.0dc5ff75.chunk.js"
+    "revision": "f037c387670ffb79d976",
+    "url": "/database-project/static/js/main.abadca61.chunk.js"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -60,16 +60,16 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/icons.8e3c7f55.eot"
   },
   {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/database-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/database-project/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/database-project/static/media/brand-icons.c5ebe0b3.ttf"
-  },
-  {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/database-project/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/database-project/static/css/2.5366cb87.chunk.css"
   },
   {
-    "revision": "170198b2378589ad9db4b0c74a702087",
+    "revision": "018c8324fb94b0b8d55b77581888fc6f",
     "url": "/database-project/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/database-project/precache-manifest.01c76a9fafac65d2e015ae67cdb1586a.js"
+  "/database-project/precache-manifest.be46cc4b649fcc5a54b7dc705a4afa1f.js"
 );
 
 workbox.clientsClaim();
