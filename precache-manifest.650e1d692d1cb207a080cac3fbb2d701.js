44b509fc0fc15a32cad1e01859c2783dcf790c89
Tidish <tidish@inbox.lt>
2019-05-14 16:52:11 +0300

    Updates

--- precache-manifest.650e1d692d1cb207a080cac3fbb2d701.js
+++ precache-manifest.650e1d692d1cb207a080cac3fbb2d701.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "2e13cae85086161125d9",
+    "revision": "21d353bced178f52fb61",
     "url": "/database-project/static/css/main.f5604eca.chunk.css"
   },
   {
@@ -44,8 +44,8 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "2e13cae85086161125d9",
-    "url": "/database-project/static/js/main.10d24f83.chunk.js"
+    "revision": "21d353bced178f52fb61",
+    "url": "/database-project/static/js/main.e15c95ee.chunk.js"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -64,16 +64,16 @@ self.__precacheManifest = [
     "url": "/database-project/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/database-project/static/media/outline-icons.82f60bd0.svg"
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/database-project/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/database-project/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/database-project/static/media/brand-icons.13db00b7.eot"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/database-project/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/database-project/static/css/2.5366cb87.chunk.css"
   },
   {
-    "revision": "99b33117271e45f41849bd8dfa894a1d",
+    "revision": "6b370f88e830e12d36cbe2ed12a8b14d",
     "url": "/database-project/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/database-project/precache-manifest.5739444311590aea36b16acf045bd4ed.js"
+  "/database-project/precache-manifest.650e1d692d1cb207a080cac3fbb2d701.js"
 );
 
 workbox.clientsClaim();
