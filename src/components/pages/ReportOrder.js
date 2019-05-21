848cc42d28d7fdd9f2de623d173f833a18fe2d0f
Tidish <tidish@inbox.lt>
2019-05-21 17:03:41 +0300

    Added datetime packages, did report order forms

--- src/components/pages/ReportOrder.js
+++ src/components/pages/ReportOrder.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Form, Button, Input } from 'semantic-ui-react';
+
+import SemanticDatepicker from 'react-semantic-ui-datepickers';
+import 'react-semantic-ui-datepickers/dist/react-semantic-ui-datepickers.css';
+
+
 class ReportOrder extends Component {
 
 
@@ -13,7 +19,7 @@ class ReportOrder extends Component {
     const errors = this.validate(this.state.data);
     this.setState({errors});
     if(Object.keys(errors).length === 0) {
-      this.updateData(this.state.data);
+      this.getItems(this.state.data);
     }
   }
 
@@ -38,15 +44,44 @@ class ReportOrder extends Component {
       this.setState( { errors: err.response.data.errors });
     });
   }
-
+//<Input icon='calendar' iconPosition='left' fluid placeholder='Date to' />
   render() {
 
     return(
       <React.Fragment>
+        <Form>
+          <Form.Group widths='equal'>
+            <Form.Field>
+              <label>Date from</label>
+              <div style={FieldWidth}>
+              <SemanticDatepicker type="basic" />
+              </div>
+            </Form.Field>
+            <Form.Field>
+              <label>Date to</label>
+              <SemanticDatepicker type="basic"></SemanticDatepicker>
 
+            </Form.Field>
+            <Form.Field>
+              <label>Restaurant</label>
+              <Input placeholder='Restaurant'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Client</label>
+              <Input placeholder='Client'/>
+            </Form.Field>
+
+          </Form.Group>
+          <Button type='submit'>Filter</Button>
+
+        </Form>
       </React.Fragment>
     );
   }
+}
+
+const FieldWidth = {
+  width: '100%'
 }
 
 export default ReportOrder;
