58256112a85537beb336ddca43254e4674389cdd
Tidish <tidish@inbox.lt>
2019-05-31 00:08:47 +0300

    Updated index

--- src/components/pages/Index.js
+++ src/components/pages/Index.js
@@ -4,6 +4,9 @@ function Index() {
     return (
       <div >
         <h1>Mysql closes the connection, it takes about 2000ms for it to reconnect, Please wait </h1>
+        <h1>It doesnt always work, because heroku crashes when mysql closes the connection.</h1>
+        <h1>For it to work it needs to be manually restarted</h1>
+        
 
       </div>
     )
