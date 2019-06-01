d45d0eecede6a61100524154076b443efe4bf1d4
Tidish <tidish@inbox.lt>
2019-06-01 20:38:51 +0300

    mistake fix

--- src/components/pages/Index.js
+++ src/components/pages/Index.js
@@ -4,7 +4,7 @@ function Index() {
     return (
       <div >
         <h1>Mysql closes the connection, it takes about 2000ms for it to reconnect, Please wait </h1>
-        <h2>Pages cannot be reloaded because it works on server-side-routing, wild card method doesn't work</h2>
+        <h2>Pages cannot be reloaded because it works on client-side-routing, wild card method doesn't work</h2>
       </div>
     )
 }
